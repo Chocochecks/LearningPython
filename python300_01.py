ace1654d5993016130c10a3b0050469170f91205
Chocochecks <146043531+Chocochecks@users.noreply.github.com>
2023-10-06 17:59:20 +0900

    초보자를 위한 파이썬 300제 1장

---
+++ python300_01.py
@@ -0,0 +1,33 @@
+#초보자를 위한 파이썬 300제 01. 파이썬 시작하기
+
+#1
+print('Hello World')
+
+#2
+print('Mary\'s cosmetics')
+
+#3
+print('신씨가 소리질렀다. \"도둑이야\".')
+
+#4
+print('C:\Windows')
+
+#5
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+#\n은 줄바꿈, \t는 탭공백
+
+
+#6
+print("오늘은", "일요일")
+
+#7
+print('naver;kakao;sk;samsung')
+
+#8
+print('naver/kakao/sk/samsung')
+
+#9
+print("first", end='');print("second")
+
+#10
+print(5/3)
